77b1d1844788b09ed6e27eaf97173d5a6320268f
Runju-Raton <runju.raton@gmail.com>
2021-02-01 00:07:24 +0600

    31-jan-2021

---
+++ majid vai/c_project/admin/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login page</title>
+    <!-- font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <!-- custom css -->
+    <link rel="stylesheet" href="../css/adminStyle.css">
+</head>
+<body>
+    <div class="sidenav">
+    <h3>Dashboard</h3>
+    <div class="conatiner">
+        <div class="row">
+            <div class="col-md-2">
+                <nav>
+                    <ul>
+                        <li><a href="#"><i class="fa fa-user"></i> Profile</a></li>
+                        <li><a href="#">Approve</a></li>
+                        <li><a href="#">Slider</a></li>
+                        <li><a href="#">Category</a></li>
+                        <li><a href="#">Product</a></li>
+                        <li><a href="#">Registration</a></li>
+                        <li><a href="#">Logout</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+
+                </nav>
+            </div>
+        </div>
+    </div>
+    
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/js/all.min.js" integrity="sha512-UwcC/iaz5ziHX7V6LjSKaXgCuRRqbTp1QHpbOJ4l1nw2/boCfZ2KlFIqBUA/uRVF0onbREnY9do8rM/uT/ilqw==" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ majid vai/c_project/index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+
+	<!--turn off IE compatibility mode -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<!-- Charecter set -->
+	<meta charset="utf-8">
+	<!-- View Port -->
+	<meta name="viewport" content="width=device-width, minimum-scale=1" />
+
+	<!-- Website Onpage SEO -->
+	<!-- this is title -->
+	<title>Our Shop Online Shopping Marketplace: Clothes, Shoes, Beauty, Electronics and More</title>
+	<!-- Keyword -->
+	<meta name="Keyword" content="online shopping, home shopping online, shop at home, gift shop, online shop, shop, online shopping mall, SHOP.COM">
+	<!-- Meta Description -->
+	<meta name="description" content="SHOP.COM Marketplace offers great deals on clothes, beauty, health and nutrition, shoes, electronics, and more from over 1,500 stores with one easy checkout.">
+
+	<!-- css for bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<!-- css for Fontawesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+
+	<!-- my css file -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+
+</head>
+<body>
+
+    <!-- This is header section -->
+    <header class="mt-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h6>Customer care: +8801721527505</h6>
+                </div>
+                <div class="col-md-6 offset-md-3">
+                    <ul class="float-right">
+                        <li class="d-inline p-2"><a href="#" class="text-center" style="color:rgba(0,0,0,.8)"><i class="fab fa-facebook"></i></a></li>
+                        <li class="d-inline p-2"><a href="#" class="text-center" style="color:rgba(0,0,0,.8)"><i class="fab fa-twitter"></i></a></li>
+                        <li class="d-inline p-2"><a href="#" class="text-center" style="color:rgba(0,0,0,.8)"><i class="fab fa-google-plus"></i></a></li>
+                        <li class="d-inline p-2"><a href="#" class="text-center" style="color:rgba(0,0,0,.8)"><i class="fab fa-pinterest"></i></a></li>
+                        <li class="d-inline p-2"><a href="#" class="text-center" style="color:rgba(0,0,0,.8)"><i class="fab fa-dribbble"></i></a></li>
+                        <li class="d-inline p-2"><a href="#" class="text-center" style="color:rgba(0,0,0,.8)"><i class="fab fa-instagram"></i></a></li>
+                        <li class="d-inline p-2"><a href="#" class="text-center" style="color:rgba(0,0,0,.8)"><i class="fab fa-linkedin"></i></a></li>
+                        <li class="d-inline p-2"><a href="#" class="text-center" style="color:rgba(0,0,0,.8)"><i class="fab fa-flickr"></i></a></li>
+                        <li class="d-inline p-2"><a href="#" class="text-center" style="color:rgba(0,0,0,.8)">My account</a></li>
+                        <li class="d-inline p-2"><a href="#" class="text-center" style="color:rgba(0,0,0,.8)">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col-md-3">
+                    <h4 class="text-uppercase" style="color:rgb(247,181,0);">Bd-Bussiness</h4>
+                    <p class="text-uppercase" style="font-size:12px;">Business-worldwide</p>
+                </div>
+                <div class="col-md-6 offset-md-3 mt-2">
+                    <!-- BUTTON ADDONS -->
+                    <div class="input-group">
+                        <input class="form-control" type="text" placeholder="Search produts..."  style="border:2px solid rgb(247,181,0);">
+                        <div class="input-group-append">
+                            <button class="pl-5 pr-5" style="background-color:rgb(247,181,0); border: 0px; box-sizing:border-box; overflow:hidden;"><i class="fa fa-search text-white"></i></button>
+                        </div>
+                        
+                            <a href="" class="pl-4 pt-2" style="font-size:18px; color:rgb(153,153,153);"><i class="far fa-heart"></i></a>
+                           <a href="" class="pl-4 pt-2" style="font-size:18px; color:rgb(153,153,153);"><i class="fas fa-shopping-basket"></i></a> 
+                           <div class="pl-4" style="color:rgb(153,153,153);">
+                           <p style="font-size:16px; margin-bottom:-10px;">Total</p>
+                            <small style="font-size:14px;">&#2547; 0.00</small>
+                           </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- nav bar here -->
+    <nav class="navbar navbar-expand-sm navbar-light bg-light  mb-3">
+        <div class="container">
+            
+            <ul class="navbar-nav bg-dark text-white">
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-home"></i> Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-list-alt"></i> Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-store-alt"></i> Shop</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Wholesale</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Intenational Business</a>
+                    <div class="dropdown-menu" id="runju">
+                        <a href="#" class="dropdown-item" >Import</a>
+                        <a href="#" class="dropdown-item">Export</a>
+                    </div>
+                </li>
+
+                 <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Marketing Communication</a>
+                    <div class="dropdown-menu"  id="runju">
+                        <a href="#" class="dropdown-item">Promotion</a>
+                        <a href="#" class="dropdown-item">Survey</a>
+                       
+                    </div>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> <i class="fas fa-envelope"></i> Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i> Cart</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+   <div class="offer">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   <img src="img/banner.jpg.jpg " alt="" class="w-100">
+               </div>
+           </div>
+       </div>
+   </div>
+   <div class="my-slider py-3">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-10">
+                    <!-- SLIDER WITH CAPTIONS -->
+                    <div id="slider4" class="carousel slide mb-5" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li class="active" data-target="#slider4" data-slide-to="0"></li>
+                            <li data-target="#slider4" data-slide-to="1"></li>
+                            <li data-target="#slider4" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img class="d-block img-fluid w-100 p-5"  src="img/slide1.jpg"  alt="First Slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block img-fluid img-fluid w-100 p-5"  src="img/slide2.jpg"  alt="Second Slide">    
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block img-fluid  img-fluid w-100 p-5" src="img/slide3.jpg" alt="Third Slide">
+                            </div>
+                        </div>
+                        <a href="#slider4" class="carousel-control-prev" data-slide="prev">
+                            <span class="carousel-control-prev-icon"></span>
+                        </a>
+
+                        <a href="#slider4" class="carousel-control-next" data-slide="next">
+                            <span class="carousel-control-next-icon"></span>
+                        </a>
+                    </div>
+               </div>
+               <div class="col-md-2">
+                   <img src="img/side1.jpg" alt="" class="img-fluid py-3">
+                   <img src="img/side2.jpg" alt="" class="img-fluid py-3">
+                   <img src="img/side3.jpg" alt="" class="img-fluid py-3">
+               </div>
+           </div>
+       </div>
+   </div>
+
+	<!-- jQuery 3.5.1 Verson File -->
+	<script type="text/javascript" src="js/jquery-3.5.1.js"></script>
+	<!-- js for bootstrap -->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<!-- js for fontawesome -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/js/all.min.js" integrity="sha512-UwcC/iaz5ziHX7V6LjSKaXgCuRRqbTp1QHpbOJ4l1nw2/boCfZ2KlFIqBUA/uRVF0onbREnY9do8rM/uT/ilqw==" crossorigin="anonymous"></script>
+    <!-- custom script -->
+	<script type="text/javascript" src="scripts.js"></script>
+
+
+</body>
+</html>
